bbf29a5c607d6961cc85f53a6436739fe4d542a0
Amol Lokhande <lokhandeamol0000@gmail.com>
2026-02-26 02:28:16 +0530

    Validation added login and sign up

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -7,6 +7,11 @@ export async function register(req, res) {
     try {
         const { name, email, password } = req.body;
 
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+
         const isPresent = await userModel.findOne({ email });
         if (isPresent) {
             return res.status(409).json({
@@ -32,6 +37,10 @@ export async function register(req, res) {
 export async function login(req, res) {
     try {
         const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
 
         const user = await userModel.findOne({ email });
         if (!user) {
