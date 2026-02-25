04ca011dedb9fd074276bf9036c05811ee7b2e42
Amol Lokhande <lokhandeamol0000@gmail.com>
2026-02-26 01:00:42 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import 'dotenv/config';
 import app from './src/app.js';
 import { connectDB } from './src/config/db.js';
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT || 3000;
 
 await connectDB();
 
-app.listen(3300, () => {
-    console.log(`Server started successfully on -- ${3300}`);
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
